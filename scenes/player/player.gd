7ded2d833c792df468f77705198dbf47851751b3
Lucas Hardman <lucas.hardman92@gmail.com>
2023-07-21 14:48:10 -0300

    Player animation

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,14 +2,32 @@ extends CharacterBody2D
 
 
 @export var movement_speed: int = 100
-
+@export var animation_player: AnimationPlayer
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("left", "right", "up", "down")
+	
+	_change_direction(direction)
 	
 	# For people using joystick
 	if direction.length() > 1:
 		direction = direction.normalized()
 	
 	velocity = direction * movement_speed
+	
 	move_and_slide()
+
+func _change_direction(direction: Vector2) -> void:
+	
+	if direction == Vector2.DOWN:
+		animation_player.play("WalkDown")
+	if direction == Vector2.UP:
+		animation_player.play("WalkUp")
+	if direction == Vector2.LEFT:
+		animation_player.play("WalkLeft")
+	if direction == Vector2.RIGHT:
+		animation_player.play("WalkRight")
+	if direction == Vector2.ZERO:
+		animation_player.stop()
+	
+	
