e28f01623ccf89cfc6c571872e9a6ad5dba51e49
thiagola92 <thiagola92@gmail.com>
2023-07-21 17:15:55 -0300

    Add light pole light

---
+++ scenes/objects/light_pole_light/light_pole_light.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+var current: int = 0
+
+@export var intervals: Array[float] = [1]
+
+
+func _ready() -> void:
+	start_next_interval()
+
+
+func start_next_interval() -> void:
+	if current >= intervals.size():
+		current = 0
+	
+	$Timer.wait_time = intervals[current]
+	$PointLight2D.visible = not $PointLight2D.visible
+	
+	current += 1
