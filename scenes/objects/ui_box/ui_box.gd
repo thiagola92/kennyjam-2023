d802cf2e7e45d475ebbf3ba5deac4753734a81e1
thiagola92 <thiagola92@gmail.com>
2023-07-22 02:23:06 -0300

    Text in another layer

--- scenes/objects/ui_box/ui_box.gd
+++ scenes/objects/ui_box/ui_box.gd
@@ -1,15 +1,19 @@
 extends Node2D
 
+
 @export var label_showing: bool = false
 
+@export var msg: Label
+
+
 func _show_label (text: String) -> void:
-	$Text.text = text
+	msg.text = text
 	var tween = get_tree().create_tween().bind_node(self)
-	tween.tween_property($Text, "visible_ratio", 1.0, 1)
+	tween.tween_property(msg, "visible_ratio", 1.0, 1)
 	label_showing = true
 	
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("interaction") and label_showing:
-		$Text.visible_ratio = 0
+		msg.visible_ratio = 0
 		label_showing = false
