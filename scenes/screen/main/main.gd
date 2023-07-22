abbbfc53bda96446f42bd54a8e98769b33d55ece
thiagola92 <thiagola92@gmail.com>
2023-07-22 19:29:22 -0300

    Add button to credits

--- scenes/screen/main/main.gd
+++ scenes/screen/main/main.gd
@@ -5,5 +5,9 @@ func start_game():
 	get_tree().change_scene_to_file("res://scenes/maps/levels/level1/level1.tscn")
 
 
+func credits():
+	get_tree().change_scene_to_file("res://scenes/screen/credits/credits.tscn")
+
+
 func exit_game():
 	get_tree().quit()
