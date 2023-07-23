c2f266b68f312825a400e4a462792057a6d8bc45
thiagola92 <thiagola92@gmail.com>
2023-07-23 05:21:53 -0300

    Can go back from credits

---
+++ scenes/screen/credits/credits.gd
@@ -0,0 +1,16 @@
+extends MarginContainer
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if (
+		Input.is_action_just_pressed("cancel")
+		or Input.is_action_just_pressed("cross")
+		or Input.is_action_just_pressed("flashlight")
+		or Input.is_action_just_pressed("interaction")
+		or Input.is_action_just_pressed("down")
+		or Input.is_action_just_pressed("up")
+		or Input.is_action_just_pressed("right")
+		or Input.is_action_just_pressed("left")
+		or Input.is_action_just_pressed("run")
+	):
+		get_tree().change_scene_to_file("res://scenes/screen/main/main.tscn")
