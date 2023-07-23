3675694f989e96cfc4050e177d7f38d3efae69c8
Stephanie Fay <stephaniemfay@gmail.com>
2023-07-22 22:48:08 -0300

    adding lights, hideouts and monster to level 3
    now it's time to run!!

--- scenes/objects/light_pole_light/light_pole_generic.gd
+++ scenes/objects/light_pole_light/light_pole_generic.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
-
 var current: int = 0
 
 @export var intervals: Array[float] = [1]
+var rng = RandomNumberGenerator.new()
 
 func _ready() -> void:
+	fill_array_randomly()
 	start_next_interval()
 
 
@@ -20,3 +21,14 @@ func start_next_interval() -> void:
 	$PointLight2D.visible = not $PointLight2D.visible
 	
 	current += 1
+
+func get_random_for_array_size() -> int:
+	return rng.randi_range(1, 15)
+
+
+func fill_array_randomly() -> void:
+	var size = get_random_for_array_size()
+	
+	while size > 0:
+		intervals.push_back(rng.randf_range(0.2, 2))
+		size = size - 1
